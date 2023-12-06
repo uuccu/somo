b06be7615969e548d43b9fb3ca27b95dbaec27f5
uuccu <sungu0804@gmail.com>
2023-12-06 22:48:26 +0900

    Update bottom_bar_routing_page.dart

--- lib/routing/bottom_bar_routing_page.dart
+++ lib/routing/bottom_bar_routing_page.dart
@@ -1,24 +1,6 @@
 import 'package:agile_frontend/page/my_home_page.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page'),
-    );
-  }
-}
-
-class SavedPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Saved Page'),
-    );
-  }
-}
-
 class BottomBarRoutingPage extends StatefulWidget {
   @override
   _BottomBarRoutingPageState createState() => _BottomBarRoutingPageState();
@@ -27,8 +9,7 @@ class BottomBarRoutingPage extends StatefulWidget {
 class _BottomBarRoutingPageState extends State<BottomBarRoutingPage> {
   ValueNotifier<int> selectedIndex = ValueNotifier<int>(0);
   List<Widget> navBarPages = [
-    MyHomePage(),
-    SavedPage(),
+    const MyHomePage(),
     // const Text('add'),
     // const MapPage(),
     // const CommunityPage(),
@@ -47,54 +28,67 @@ class _BottomBarRoutingPageState extends State<BottomBarRoutingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(leading: BackButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )),
-      body: AnimatedBuilder(
-        animation: selectedIndex,
-        builder: (BuildContext context, Widget child) {
-          return navBarPages[selectedIndex.value];
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          // Lim Chae Yeong
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark),
-            label: 'Saved',
-            backgroundColor: Colors.black,
-          ),
-          //BottomNavigationBarItem(
-          //icon: Icon(Icons.add),
-          //label: 'Add',
-          //backgroundColor: Colors.black,
-          //),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            label: 'Map',
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.access_time_filled_sharp),
-            label: 'Community',
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-            backgroundColor: Colors.black,
-          ),
-        ],
-        currentIndex: selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+      body: navBarPages[selectedIndex.value],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            // Lim Chae Yeong
+            BottomNavigationBarItem(
+              icon: Image(image: AssetImage('asset/image/bottom_bar/home.png')),
+              activeIcon:
+                  Image(image: AssetImage('asset/image/bottom_bar/home2.png')),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon:
+                  Image(image: AssetImage('asset/image/bottom_bar/saved.png')),
+              activeIcon:
+                  Image(image: AssetImage('asset/image/bottom_bar/saved2.png')),
+              label: 'Saved',
+            ),
+            BottomNavigationBarItem(
+              icon: Image(
+                  image: AssetImage('asset/image/bottom_bar/map_icon.png')),
+              activeIcon: Image(
+                  image: AssetImage('asset/image/bottom_bar/map_icon2.png')),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: Image(
+                  image:
+                      AssetImage('asset/image/bottom_bar/community_icon.png')),
+              activeIcon: Image(
+                  image:
+                      AssetImage('asset/image/bottom_bar/community_icon2.png')),
+              label: 'Community',
+            ),
+            BottomNavigationBarItem(
+              icon: Image(
+                  image: AssetImage('asset/image/bottom_bar/profile.png')),
+              activeIcon: Image(
+                  image: AssetImage('asset/image/bottom_bar/profile2.png')),
+              label: 'Profile',
+            ),
+          ],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: selectedIndex.value,
+          selectedItemColor: Colors.amber[800],
+          unselectedItemColor: Colors.black,
+          showUnselectedLabels: true,
+          backgroundColor: Colors.white,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
