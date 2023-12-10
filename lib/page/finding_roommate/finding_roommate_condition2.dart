abfea03e2f8a76fd551d2dcc82799d1f9636e35a
uuccu <sungu0804@gmail.com>
2023-12-10 22:46:43 +0900

    feat: integration complete

---
+++ lib/page/finding_roommate/finding_roommate_condition2.dart
@@ -0,0 +1,140 @@
+import 'package:agile_frontend/page/finding_roommate/finding_roommate_condition3.dart';
+import 'package:agile_frontend/util/device/screen.dart';
+import 'package:agile_frontend/widget/life_style_button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class FindingRoommateConditionPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: Screen.designToScreenWidth(context, 20),
+          right: Screen.designToScreenWidth(context, 20),
+          top: Screen.designToScreenHeight(context, 100),
+          bottom: Screen.designToScreenHeight(context, 30),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Share your living preferences',
+              style: TextStyle(
+                fontSize: 40,
+                fontFamily: 'hgg',
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 8)),
+            const Text(
+              'Meet roommates that suits you',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'hgg',
+                color: Colors.grey,
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 30)),
+            const Text(
+              'Which lifestyle suits you better?',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.orange,
+                fontFamily: 'hgg',
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 30)),
+            const LifestyleOptionCard(
+              type: 'Mornin Person',
+              emoji: '🌞',
+              description:
+                  "I wake up early to start my day and go to bed before it gets late.",
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 16)),
+            const Text(
+              'VS',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 16)),
+            const LifestyleOptionCard(
+              type: 'Night Owl',
+              emoji: '🌛',
+              description:
+                  "I wake up a bit later and engage in activities or work into the evening. ",
+            ),
+            const Spacer(),
+            const Text(
+              'For a better match, try not to use this \'skip\' too often.',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(Size(
+                      Screen.designToScreenWidth(context, 180),
+                      Screen.designToScreenHeight(context, 40),
+                    )),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(color: Colors.grey)),
+                    alignment: Alignment.center,
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+                  ),
+                  child: const Text("Skip"),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(
+                        left: Screen.designToScreenWidth(context, 11))),
+                OutlinedButton(
+                  onPressed: () {
+                    Get.to(FindingRoommateConditionPage3());
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(Size(
+                      Screen.designToScreenWidth(context, 180),
+                      Screen.designToScreenHeight(context, 40),
+                    )),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(
+                            color: Color.fromRGBO(254, 142, 72, 1))),
+                    alignment: Alignment.center,
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(254, 142, 72, 1)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+                  ),
+                  child: const Text("Next"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/page/finding_roommate/finding_roommate_condition5.dart
@@ -0,0 +1,139 @@
+import 'package:agile_frontend/page/finding_roommate/finding_roommate_condition2.dart';
+import 'package:agile_frontend/util/device/screen.dart';
+import 'package:agile_frontend/widget/life_style_button.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class FindingRoommateConditionPage5 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          left: Screen.designToScreenWidth(context, 20),
+          right: Screen.designToScreenWidth(context, 20),
+          top: Screen.designToScreenHeight(context, 100),
+          bottom: Screen.designToScreenHeight(context, 30),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Share your living preferences',
+              style: TextStyle(
+                fontSize: 40,
+                fontFamily: 'hgg',
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 8)),
+            const Text(
+              'Meet roommates that suits you',
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: 'hgg',
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 30)),
+            const Text(
+              'Are you sensitive to indoor room temperature?',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'hgg',
+                color: Colors.orange,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 30)),
+            const LifestyleOptionCard(
+              type: 'Temperature Sensitive Type ',
+              emoji: '🥵🥶',
+              description:
+                  "I can't tolerate hot or cold weather well and am sensitive to room temperature",
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 16)),
+            const Text(
+              'VS',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 16)),
+            const LifestyleOptionCard(
+              type: 'Temperature Tolerant Type',
+              emoji: '😐',
+              description:
+                  "I can withstand hot or cold weather well and am not sensitive to room temperature.",
+            ),
+            const Spacer(),
+            const Text(
+              'For a better match, try not to use this \'skip\' too often.',
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Screen.designToScreenHeight(context, 20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(Size(
+                      Screen.designToScreenWidth(context, 180),
+                      Screen.designToScreenHeight(context, 40),
+                    )),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(color: Colors.grey)),
+                    alignment: Alignment.center,
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+                  ),
+                  child: const Text("Skip"),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(
+                        left: Screen.designToScreenWidth(context, 11))),
+                OutlinedButton(
+                  onPressed: () {
+                    Get.to(FindingRoommateConditionPage2());
+                  },
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(Size(
+                      Screen.designToScreenWidth(context, 180),
+                      Screen.designToScreenHeight(context, 40),
+                    )),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        const BorderSide(
+                            color: Color.fromRGBO(254, 142, 72, 1))),
+                    alignment: Alignment.center,
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color.fromRGBO(254, 142, 72, 1)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+                  ),
+                  child: Text("Next"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
